6c9968817ea7dd270e4fc8aa02b6d1141cb7667b
OSANAI Noriaki <osanainoriaki@gmail.com>
2016-07-08 07:33:16 +0000

    added login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   get 'signup', to: 'users#new'
 
+  resources :sessions, only: [ :new, :create, :destroy ]
   resources :users
   
   # The priority is based upon order of creation: first created -> highest priority.
