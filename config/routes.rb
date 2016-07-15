17f9e3408e60fe12a167005f6954a04a8ab95392
OSANAI Noriaki <osanainoriaki@gmail.com>
2016-07-15 03:35:22 +0000

    added followings, followers

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   resources :relationships, only: [:create, :destroy]
   resources :sessions, only: [ :new, :create, :destroy ]
   resources :users
+  get '/followers/:id', to: 'users#followers', as: 'follower'
+  get '/followings/:id', to: 'users#followings', as: 'following'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
