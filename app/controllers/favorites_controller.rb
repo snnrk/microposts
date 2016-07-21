6ccffebc1d4ea29990fa3cb49bfd096497f53c0b
OSANAI Noriaki <osanainoriaki@gmail.com>
2016-07-21 14:06:05 +0000

    added pages for favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -10,4 +10,12 @@ class FavoritesController < ApplicationController
         @micropost = current_user.favorite_relationships.find(params[:id]).micropost
         current_user.unfavorite(@micropost)
     end
+    
+    def show
+        @user = User.find(params[:id])
+        @micropost = @user.microposts.build
+        @feed_items = @user.favorite_microposts.order(created_at: :desc)
+        @title = t('dictionary.words.favorites')
+        render 'static_pages/home'
+    end
 end
